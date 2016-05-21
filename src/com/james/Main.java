c51391239d24174cb0582ee6ead4899c7a59caf4
jamesyburr <vint.james@gmail.com>
2016-05-21 13:31:21 -0400

    Updated assignment for 5/23

--- src/com/james/Main.java
+++ src/com/james/Main.java
@@ -1,22 +1,77 @@
 package com.james;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner scanner = new Scanner(System.in);
-    public static User user = new User ();
+    //
+    static HashMap<String, User> users = new HashMap<>();
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception{
-        System.out.println("Hello, What is your name?");
 
-        User user = new User ();
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Hello, What is your name?");
 
-        user.chooseName();
-        user.chooseOption();
-        user.chooseAmount();
+
+            String name = scanner.nextLine();
+
+            if (!users.containsKey(name)) {
+                System.out.println("User does not exist, would you like to create an account? [y/n]");
+                String input = scanner.nextLine();
+
+                if (input.equalsIgnoreCase("y")) {
+                    users.put(name, new User(name, 100.0));
+                }
+            }
+
+            User user = users.get(name);
+
+            if (user != null) {
+
+
+                while (true) {
+                    System.out.println("What would you like to do? [Check my balance[1], Withdraw funds[2], Cancel[3], Remove account[4]]");
+                    String option = Main.scanner.nextLine();
+
+                    if (option.equalsIgnoreCase("1")) {
+                        System.out.println("Your balance is " + user.getBalance());
+                    } else if (option.equalsIgnoreCase("2")) {
+                        System.out.println("How much would you like?");
+                        String input = scanner.nextLine();
+                        double inputNum = Double.valueOf(input);
+
+                        if (!(inputNum < 0) && !(inputNum > user.getBalance())) {
+                            double remainder = user.changeBalance(inputNum);
+                            System.out.println("Your balance is " + remainder);
+                            System.out.println("Please collect your money");
+                        } else {
+                            System.out.println("Invalid entry");
+                        }
+
+                    } else if (option.equalsIgnoreCase("3")) {
+                        System.out.println("Are you sure? [y/n]");
+                        String input = scanner.nextLine();
+
+                        if (input.equalsIgnoreCase("y")) {
+                            System.out.println("Thank you come again!");
+                            break;
+                        }
+                    } else if (option.equalsIgnoreCase("4")) {
+                        //remove account
+                        System.out.println("Are you sure [y/n]");
+                        String input = scanner.nextLine();
+
+                        if (input.equalsIgnoreCase("y")) {
+                            users.remove(user.getName());
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
     }
-
-
 }
 
 
--- src/com/james/User.java
+++ src/com/james/User.java
@@ -1,52 +1,38 @@
 package com.james;
 
-import java.util.Scanner;
 
 /**
  * Created by jamesyburr on 5/18/16.
  */
 public class User {
     String name;
-    String option;
-    String amount;
+    Double balance;
 
-    public void chooseName() throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
-
-        String name = scanner.nextLine();
-
-        if (!name.isEmpty()) {
-            System.out.println("Hello " + name);
-        } else {
-            throw new Exception("Invalid Entry");
-        }
-
-    }
-    public void chooseOption() {
-        System.out.println("What would you like to do? [Check my balance, Withdraw funds, Cancel]");
-        String option = Main.scanner.nextLine();
-
-        if (option.equalsIgnoreCase("check my balance")) {
-            System.out.println("Your balance is 100");
-        } else if (option.equalsIgnoreCase("cancel")) {
-            System.out.println("Thank you and please come again!");
-        } else if (option.equalsIgnoreCase("withdraw funds")) {
-            System.out.println("How much would you like?");
-        }
+    public User(String name, Double balance) {
+        this.name = name;
+        this.balance = balance;
     }
 
-    public void chooseAmount() throws Exception {
+    public String getName() {
+        return name;
+    }
 
-        String amount = Main.scanner.nextLine();
-        int num = Integer.valueOf(amount);
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        if (num <= 100) {
-            System.out.println("Please collect your money");
-            System.out.println("Have a nice day!");
-        } else if (num > 100) {
-            throw new Exception("Insufficient funds");
-        }
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double changeBalance(double amnt) {
+        balance -= amnt;
+        return balance;
     }
 }
 
